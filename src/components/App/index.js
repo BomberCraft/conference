c19f52045b1f7b1d5c44a852fae9127bdef8ec9a
yohann.poli <yohann.poli@gmail.com>
2017-10-23 20:40:40 +0200

    save data to localStorage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import {compose} from 'recompose';
 import CircularProgress from 'material-ui/Progress/CircularProgress';
 import {withStyles} from 'material-ui/styles';
 import PropTypes from 'prop-types';
+import Connection from 'cordova-plugin-network-information/www/Connection';
 import fetchData from '../../fetchData';
 import Header from '../Header';
 import Main from '../Main';
@@ -24,6 +25,8 @@ export class App extends React.Component {
     entities: PropTypes.object,
   };
 
+  static storageKey = 'entities';
+
   getChildContext() {
     return {entities: this.state.entities};
   }
@@ -34,15 +37,64 @@ export class App extends React.Component {
     entities: null,
   };
 
+  constructor(props) {
+    super(props);
+
+    this.handleOnline = this.handleOnline.bind(this);
+  }
+
   componentWillMount() {
+    const {navigator, cordova} = window;
+
+    if (cordova && navigator.connection.type === Connection.NONE) {
+      const entities = localStorage.getItem(App.storageKey);
+
+      if (entities) {
+        this.handleData(JSON.parse(entities));
+      } else {
+        const error = new Error('Failed to load data.');
+
+        this.handleError(error);
+      }
+    } else {
+      this.handleFetch();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('online', this.handleOnline, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.handleOnline, false);
+  }
+
+  handleFetch() {
     fetchData()
       .then(entities => {
-        setTimeout(() => {
-          this.setState({entities, isLoading: false});
-          this.props.history.replace('/');
-        }, 800);
+        this.handleData(entities);
+        this.saveData(entities);
       })
-      .catch(error => this.setState({error, isLoading: false}));
+      .catch(error => this.handleError(error));
+  }
+
+  saveData(entities) {
+    localStorage.setItem(App.storageKey, JSON.stringify(entities));
+  }
+
+  handleData(entities) {
+    setTimeout(() => {
+      this.setState({entities, isLoading: false});
+      this.props.history.replace('/');
+    }, 800);
+  }
+
+  handleError(error) {
+    this.setState({error, isLoading: false});
+  }
+
+  handleOnline() {
+    this.handleFetch();
   }
 
   render() {
